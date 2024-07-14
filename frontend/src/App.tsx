46b22d286bc7a05e630fb3d8f82d08f9ef8ce42e
cassiofelipe <cassiofscosta@gmail.com>
2024-07-14 19:49:03 -0300

    Adicionada primeira chamada de busca das receitas.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,34 @@
+import { FormEvent, useState } from "react";
 import "./App.css";
+import  * as api from "./api"
+import { Recipe } from "./types";
 
 const App = () => {
-  return <div>Hello from recipe app!</div>
+  const [searchTerm, setSearchTerm] = useState("Lentil");
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  const handleSearchSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const recipes = await api.searchRecipes(searchTerm, 1);
+      setRecipes(recipes.results);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return <div>
+    <form onSubmit={(event) => handleSearchSubmit(event)}>
+      <button>Submit</button>
+    </form>
+    {recipes.map((recipe) => (
+      <div>
+        recipe image location: {recipe.image};
+        recipe title: {recipe.title};
+      </div>
+    ))}
+  </div>
 };
 
 
